94ccb40ca466067d46ae2168ee2a4e08bd94f529
한태규 <gksxorb147@gmail.com>
2022-02-12 15:59:47 +0900

    article_detail REST 생성 - GET - PUT - DELETE

--- Practice1/MyProject/api_basic/views.py
+++ Practice1/MyProject/api_basic/views.py
@@ -5,6 +5,7 @@ from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
 
+
 @csrf_exempt
 def article_list(request):
 
@@ -20,4 +21,31 @@ def article_list(request):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.data, status=400)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def article_detail(request, pk):
+
+    try:
+        articles = Article.objects.get(pk=pk)
+
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(articles)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(articles, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        articles.delete()
+        return HttpResponse(status=204)
