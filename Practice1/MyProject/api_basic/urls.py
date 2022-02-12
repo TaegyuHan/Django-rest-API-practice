f3e40d48f6f7bf4800fcb695c6163a9aea2a27df
한태규 <gksxorb147@gmail.com>
2022-02-12 18:32:16 +0900

    ArticleViewSet 생성

--- Practice1/MyProject/api_basic/urls.py
+++ Practice1/MyProject/api_basic/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
-from .views import article_list, article_detail
-from .views import ArticleAPIView, ArticleDetailAPIView, GenericAPIView
+from django.urls import path, include
+# from .views import article_list, article_detail # 함수 사용
+from .views import (
+    ArticleAPIView, ArticleDetailAPIView,
+    GenericAPIView, ArticleViewSet
+)
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
+
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>', include(router.urls)),
+
     # path('article/', article_list), # 함수 버전
     # path('detail/<int:id>', article_detail),
 
