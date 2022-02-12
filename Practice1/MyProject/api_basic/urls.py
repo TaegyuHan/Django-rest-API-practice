13ec5d9295124c393539a11e0f838fa710cfbf55
한태규 <gksxorb147@gmail.com>
2022-02-12 16:52:05 +0900

    API 클래스 버전

--- Practice1/MyProject/api_basic/urls.py
+++ Practice1/MyProject/api_basic/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import article_list, article_detail
+from .views import ArticleAPIView, ArticleDetailAPIView
+
 
 urlpatterns = [
-    path('article/', article_list),
-    path('detail/<int:pk>', article_detail),
+    # path('article/', article_list), # 함수 버전
+    # path('detail/<int:id>', article_detail), # 함수 버전
+
+    path('article/', ArticleAPIView.as_view()), # 클래스 버전
+    path('detail/<int:id>', ArticleDetailAPIView.as_view()), # 클래스 버전
+
 ]
--- Practice1/MyProject/api_basic/views.py
+++ Practice1/MyProject/api_basic/views.py
@@ -7,12 +7,62 @@ from rest_framework.decorators import api_view # 부트스트랩 형태로 보�
 from rest_framework.response import Response
 from rest_framework import status
 
+from rest_framework.views import APIView # class 로 만들기
+
 from .models import Article
 from .serializers import ArticleSerializer
 
 
+class ArticleAPIView(APIView):
+    """  ArticleAPIView 클래스 버전 """
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetailAPIView(APIView):
+    """  ArticleDetailAPIView 클래스 버전 """
+
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+
+        except Article.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
 @api_view(['GET', 'POST'])
 def article_list(request):
+    """  article_list 함수 버전 """
 
     if request.method == "GET":
         articles = Article.objects.all()
@@ -25,24 +75,25 @@ def article_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT' , 'DELETE'])
-def article_detail(request, pk):
+def article_detail(request, id):
+    """  article_detail 함수 버전 """
 
     try:
-        articles = Article.objects.get(pk=pk)
+        article = Article.objects.get(id=id)
 
     except Article.DoesNotExist:
         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = ArticleSerializer(articles)
+        serializer = ArticleSerializer(article)
         return Response(serializer.data)
 
     elif request.method == 'PUT':
-        serializer = ArticleSerializer(articles, data=request.data)
+        serializer = ArticleSerializer(article, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
@@ -50,5 +101,5 @@ def article_detail(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
-        articles.delete()
+        article.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
