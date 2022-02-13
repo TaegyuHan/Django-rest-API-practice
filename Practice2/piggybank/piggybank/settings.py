7a49c1f7741fd9b14c66e62a54ac0feff55aa521
한태규 <gksxorb147@gmail.com>
2022-02-13 23:17:47 +0900

    Feat: Currency, Category, Transaction 모델 생성 (#123)
    모델 생성
        - Currency
        - Category
        - Transaction
    
    해결: close #123

--- Practice2/piggybank/piggybank/settings.py
+++ Practice2/piggybank/piggybank/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 추가한 APP
+    'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +79,14 @@ WSGI_APPLICATION = 'piggybank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_practice2',
+        'USER': 'root',
+        'PASSWORD': 'filecoin',
+        'HOST': '127.0.0.1',
+        'POST': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
